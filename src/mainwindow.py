a7433e39dd7fab7e021cdf9ad76c0ffbede9c974
muhhanan <16521166@std.stei.itb.ac.id>
2022-11-19 23:24:19 +0700

    gui udah jalan

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -6,11 +6,15 @@ from PIL import ImageTk, Image
 from main import *
 import os
 
+#=================================================================#
+
 def start():
-    if disphoto != '' and folderonly != '' :
-        print("Button Clicked")
+    if new != '' and folderonly != '' :
+        # print("Button Clicked")
+        global disresult, opres
+
         closestres, exetime = runprogram(folderdirac, filename)
-        discr = folderdirac + '/' + closestres
+        discr = str(folderdirac) + '/' + closestres
         if len(closestres) > 18 :
             closestres = closestres[:18]
         
@@ -18,57 +22,63 @@ def start():
         canvas.itemconfig(fcresult ,text = closestres)
 
         # print(discr)
-        imeg2 = Image.open(discr)
-        resized2 = imeg2.resize((256,256), Image.ANTIALIAS)
-        photocr2 = ImageTk.PhotoImage(resized2)
-        disphoto2 = canvas.create_image(915,387, image=photocr2)
-        disphoto2.tkraise() # masih masalah disini
+        opres = Image.open(discr)
+        reszopres = opres.resize((256,256), Image.ANTIALIAS)
+        disresult = ImageTk.PhotoImage(reszopres)
+        frame2 = Frame(window, width= 256, height= 256)
+        frame2.pack()
+        frame2.place(x= 787, y= 259, anchor=NW)
+
+        labelres = Label(frame2,image=disresult)
+        labelres.pack()
 
     else :
         messagebox.showerror("Error","Anda belum memilih folder dan file nya!")
 
 #=================================================================#
+
 def cfile():
-        global disphoto
+        global new, imeg
         global filename
         filename = ''
-        disphoto = ''
+        new = ''
 
         filename = filedialog.askopenfilename()
-        head, tail = os.path.split(filename)
-        #print(tail, "aisjdiasijdiajs")
+        head, tail = os.path.split(filename) # tail = nama file tanpa direct
+        
         if tail != '' :
-            canvas.itemconfig(NoFilC, text = tail)
+            copytail = tail # Buat display nama file tak lebih dari 22 char.
+            if len(copytail) > 22 :
+                copytail = copytail[:22]
+            canvas.itemconfig(NoFilC, text = copytail)
 
             # Load image
             imeg = Image.open(filename)
             resized = imeg.resize((256,256), Image.ANTIALIAS)
             new = ImageTk.PhotoImage(resized)
-            
-            disphoto = canvas.create_image(541,387,image=new)
-            disphoto.tkraise()
+            frame1 = Frame(window, width= 256, height= 256)
+            frame1.pack()
+            frame1.place(x= 413, y= 259, anchor=NW)
+
+            labeldis = Label(frame1,image=new)
+            labeldis.pack()
+
 #=================================================================#
-def cfolder(): #adaprngeloop terus
+
+def cfolder(): 
         global folderdirac, folderonly
         folderonly = ''
         folderdirac = ''
 
-        root = Tk()
-        root.withdraw()
         folderdirac = filedialog.askdirectory() 
-        print(folderdirac)
+        # print(folderdirac)
         folderonly = os.path.basename(folderdirac)
         if folderonly != '' :
-            canvas.itemconfig(NoFolC ,text = folderonly)
+            copyfolnly = folderonly # Buat display nama folder tak lebih dari 22 char.
+            if len(copyfolnly) > 22 :
+                copyfolnly = copyfolnly[:22]
+            canvas.itemconfig(NoFolC ,text = copyfolnly)
 
-        # Load image in folder
-        '''array_image.clear()
-        for matrix in baca_folder(folder):
-            array_image.append(matrix)
-        print(f"{len(array_image)} pictures loaded, first element:")
-        mattest = np.array(array_image[0])
-        print(f"Size: {mattest.shape}")
-        print(mattest)'''
 #=================================================================#
 
 window = Tk()
@@ -77,60 +87,57 @@ window.configure(bg = "#FFFFFF")
 window.title("Face Recognition")
 window.resizable(False, False)
 
+ico = Image.open('./srcimg/iconnya.png')
+icon = ImageTk.PhotoImage(ico)
+window.wm_iconphoto(False, icon)
+
 # Sementara
 folderonly = ''
 disphoto = ''
 
-canvas = Canvas(
-    window,
-    bg = "#FFFFFF",
-    height = 720,
-    width = 1280,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge")
+canvas = Canvas(window,
+        bg = "#FFFFFF",
+        height = 720,
+        width = 1280,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge")
 canvas.place(x = 0, y = 0)
 
-bg_img = PhotoImage(file = f"background.png")
+bg_img = PhotoImage(file = f"./srcimg/background.png")
 background = canvas.create_image(
     640.0, 360.0,
     image=bg_img)
 
-img0 = PhotoImage(file = f"btnstart.png")
-b0 = Button(
-    image = img0,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = start,
-    relief = "flat")
-b0.place(
-    x = 15, y = 592,
-    width = 287,
-    height = 105)
+img0 = PhotoImage(file = f"./srcimg/btnstart.png")
+b0 = Button(image = img0,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = start,
+        relief = "flat")
+b0.place(x = 15, y = 592,
+        width = 287,
+        height = 105)
 
-img1 = PhotoImage(file = f"btncfile.png")
-b1 = Button(
-    image = img1,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = cfile,
-    relief = "flat")
-b1.place(
-    x = 68, y = 450,
-    width = 186,
-    height = 63)
+img1 = PhotoImage(file = f"./srcimg/btncfile.png")
+b1 = Button(image = img1,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = cfile,
+        relief = "flat")
+b1.place(x = 68, y = 450,
+        width = 186,
+        height = 63)
 
-img2 = PhotoImage(file = f"btncfolder.png")
-b2 = Button(
-    image = img2,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = cfolder,
-    relief = "flat")
-b2.place(
-    x = 59, y = 168,
-    width = 204,
-    height = 64)
+img2 = PhotoImage(file = f"./srcimg/btncfolder.png")
+b2 = Button(image = img2,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = cfolder,
+        relief = "flat")
+b2.place(x = 59, y = 168,
+        width = 204,
+        height = 64)
 
 NoFolC = canvas.create_text(155,247,
                             text="No Folder Chosen",
