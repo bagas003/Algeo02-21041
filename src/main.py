046c0d41182bd43d346029f08d83ab3da1a7a46d
bagas003 <bagasaryo03@gmail.com>
2022-11-22 19:48:19 +0700

    komen

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import cv2, os, numpy, time
 import eigen
 
+# mengambil dataset dari foldername dan mengembalikan matriks berisi vektor-vektor gambar
 def getDataset(foldername):
     data = []
     for imgname in os.listdir(foldername):
@@ -9,38 +10,47 @@ def getDataset(foldername):
         data += [numpy.array(img.T).flatten()]
     return data
 
-def getCovarian(dataset):
-    return numpy.divide(numpy.dot(dataset,numpy.transpose(dataset)), len(dataset))
+# mengembalikan matriks C' (kovarian') dari dataset yang sudah dikurangkan oleh rata-rata
+def getCovarian(subtracted):
+    return numpy.divide(numpy.dot(subtracted,numpy.transpose(subtracted)), len(subtracted))
 
+# menampilkan gambar arr dalam file
 def writeImage(arr, name):
     arr = numpy.reshape(arr, (256,256))
     cv2.imwrite(name, arr.T)
 
-def getEigenfaces(dataset, v):
+# mengembalikan eigenfaces dari subtracted dataset dan evector v
+def getEigenfaces(subtracted, v):
     ui = []
     row = len(v)
     for i in range(row):
-        ui += [numpy.dot(numpy.transpose(dataset), v[:,i])]
+        ui += [numpy.dot(numpy.transpose(subtracted), v[:,i])]
     return numpy.matrix(ui)
 
+# mengembalikan vektor omega dari eigenfaces dan vektor subtracted
 def getOmega(eigenFaces, subtractedArr):
     omega = []
     for eface in eigenFaces:
         omega += [numpy.dot(eface, subtractedArr)]
     return numpy.array(omega).flatten()
 
+# mengembalikan matriks vektor omega dari eigenfaces dan matriks subtracted vektor
 def getOmegaSet(eigenfaces, subtracted):
     omegas = []
     for sub in subtracted:
         omegas += [getOmega(eigenfaces, sub)]
     return numpy.matrix(omegas)
 
-def getEuclidean(omegaSet, omegaNew):
-    euclidean = []
-    for omega in omegaSet:
-        euclidean += [getDistance(omegaNew, omegaNew)]
-    return euclidean
+# mengembalikan nilai euclidean distance antara 2 vektor
+def getDistance(vector1, vector2):
+    ret = 0
+    vector = numpy.array(vector1 - vector2).flatten()
+    for i in vector:
+        ret += i*i
+    return int(numpy.sqrt(ret))
 
+# mengembalikan euclidean distance terpendek dan index closest result dari
+# matriks omega dataset dan omega testing face baru
 def getEuclideanAndIndex(omegaSet, omegaNew):
     ed = getDistance(omegaNew, omegaSet[0])
     idx = 0
@@ -52,9 +62,11 @@ def getEuclideanAndIndex(omegaSet, omegaNew):
             idx = i
     return ed, idx
 
+# mengembalikan nama file di foldername sesuai index
 def getFileName(foldername, index):
     return os.listdir(foldername)[index]
 
+# mengembalikan threshold dari omega dataset
 def getThreshold(omegaset):
     l = len(omegaset)
     threshold = 0
@@ -63,84 +75,80 @@ def getThreshold(omegaset):
             threshold = max(threshold, getDistance(omegaset[i], omegaset[j]))
     return threshold / 4
 
-def getDistance(vector1, vector2):
-    ret = 0
-    vector = numpy.array(vector1 - vector2).flatten()
-    for i in vector:
-        ret += i*i
-    return int(numpy.sqrt(ret))
 
-
-def runprogram(foldername, filename): #keduanya dirac full 
+# pemanggilan program utama
+def runprogram(foldername, filename):
     start = time.time()
-
+    '''
+    ----DATASET FOLDER-----           ---MATRIKS VEKTOR DATASET (D)---
+     img 1   img 2   img 3
+    [A A A] [B B B] [C C C]             [ [A A A A A A A A A],
+    [A A A] [B B B] [C C C]     ---->     [B B B B B B B B B],
+    [A A A] [C C C] [C C C]               [C C C C C C C C C] ]
+    '''
     dataset = getDataset(foldername) 
-    mean = numpy.mean(dataset, axis=0) 
-    subtracted = dataset - mean 
-    
+    mean = numpy.mean(dataset, axis=0) # mean =  [x x x x x x x x]
+    '''
+    MATRIKS SUBTRACTED DATASET (A)
+              A = D - mean
+    [ [A1 A1 A1 A1 A1 A1 A1 A1 A1],
+      [B1 B1 B1 B1 B1 B1 B1 B1 B1],
+      [C1 C1 C1 C1 C1 C1 C1 C1 C1] ]
+    '''  
+    subtracted = dataset - mean
+      
+    '''
+    Covarian' (C')
+    Covarian = A x A.Transpose ; Covarian' = A.Transpose x A
+    '''
     covarian = getCovarian(subtracted)
-    # evalues, evectors = numpy.linalg.eigh(covarian)
+    '''
+    C' x v(i) = lambda(i) x v(i)
+    dengan: lambda = eigenvalues
+                 v = eigenvector
+    '''
     evectors = eigen.getEVEV(covarian)
     
+    '''
+    eigenfaces (u)
+    u(i) = A x v(i)
+    '''
     efaces = getEigenfaces(subtracted, evectors) 
+    '''
+    omega(i) = sigma( u(k) x A(i) )
+    '''
     omegaset = getOmegaSet(efaces, subtracted) 
 
+    # membaca dan memproses testface baru menjadi vektor
     testface = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) 
     testface = cv2.resize(testface, (256, 256)) 
     testface = numpy.array(testface.T).flatten()
+
+    # mendapatkan ANew
     subtracted_test = testface - mean
 
+    # mendapatkan omegaNew
     omega = getOmega(efaces, subtracted_test)
-    # euclidean = getEuclidean(omegaset, omega)
-    # ed = min(euclidean);
-    # threshold = max(euclidean)
-    # index = numpy.argmin(euclidean)
+
+    '''
+    euclidean distance (ed)
+    ed = sqrt( sigma(i=1,n)((y(i) - x(i))^2) )
+    '''
+    # mendapatkan euclidean distance dan indeks closest result
     ed, index = getEuclideanAndIndex(omegaset, omega)
 
-    closestresult = getFileName(foldername, index) # nama file hasil similarity
-
+    # mendapatkan file closestresult
+    closestresult = getFileName(foldername, index) 
     
+    '''
+    threshold (t)
+    t = max( ed(omega(j) - omega(k)) ) ; j,k = 1,2,3,...M
+    '''
+    # mendapatkan threshold dan similarity
     threshold = getThreshold(omegaset)
     similarity = (threshold - ed) * 100 / threshold
     
     timetaken = time.time() - start
-    #===== sementara =====#
-    # print('\nclosest result for',foldername, ':',closestresult)
-    # print('time taken: ', timetaken) 
 
     return closestresult, timetaken, similarity
 
-'''
-#datasetfolder = input('enter dataset folder: (ex. newdataset)\n>> ')
-#testfacefile = input('enter testface file: (ex. test.jpg)\n>> ')
-
-# start = time.time() ini nanti dipindahin ke start button
-
-#datasetfolder = r'..\test\\' + datasetfolder diganti sama folderdirec dari cfolder
-dataset = getDataset(datasetfolder) # jadi matriks dari semua foto yang ada di dataset
-mean = numpy.mean(dataset, axis=0) 
-subtracted = dataset - mean 
-
-covarian = getCovarian(subtracted)
-evalues, evectors = numpy.linalg.eigh(covarian) # ini punya si farhan
-
-efaces = getEigenfaces(subtracted, evectors) 
-omegaset = getOmegaSet(efaces, subtracted) 
-
-testface = cv2.imread(r'..\test\testface_folder\\' + testfacefile, cv2.IMREAD_GRAYSCALE)
-testface = cv2.resize(testface, (256, 256))
-testface = numpy.array(testface.T).flatten()
-subtracted_test = testface - mean
-
-omega = getOmega(efaces, subtracted_test)
-
-ed, index = getEuclideanAndIndex(omegaset, omega)
-
-
-closestresult = getFileName(datasetfolder, index)
-# print('\nclosest result for',testfacefile, ':',closestresult)
-
-# print('time taken: ', time.time() - start) nanti ditaro di ex time 
-
-# writeImage(dataset[index], '../test/result.jpg')
-# writeImage(testface, '../test/testface.jpg')'''
